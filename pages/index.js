79e92889ec895704812e9093d33ff0bd2b5a5c92
yaseen-nexevo <yaseen.nexevo@gmail.com>
2022-09-22 17:30:30 +0530

    wef

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Something Big is brewing!!!
+          Something Big is brewing!!!!!
         </h1>
 
         <p className={styles.description}>
